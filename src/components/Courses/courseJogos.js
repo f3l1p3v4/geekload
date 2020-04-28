9693cbb80ff8fb5d1a3504bcfc5949ca1a7458a6
f3l1p3v4 <fvaldez018@gmail.com>
2020-04-28 13:56:30 -0400

    Altera endereço api

--- src/components/Courses/courseJogos.js
+++ src/components/Courses/courseJogos.js
@@ -14,6 +14,8 @@ export default function Courses() {
                 headers: { category }
             });
 
+            alert(response.data);
+
             setCourses(response.data);
         }
 
