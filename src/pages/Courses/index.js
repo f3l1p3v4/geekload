971f1f379afc7213f52d8943dd86687c421a7324
f3l1p3v4 <fvaldez018@gmail.com>
2020-05-16 18:32:28 -0400

    Adiciona slider

--- src/pages/Courses/index.js
+++ src/pages/Courses/index.js
@@ -1,16 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import api from '../../services/api';
 
 import Header from '../../components/header';
 import Course from '../../components/course';
 import Footer from '../../components/footer';
 
-import arcade from '../../assets/featured/capaSite.png';
+import slide1 from '../../assets/featured/slide1.jpg';
+import slide2 from '../../assets/featured/slide2.jpg';
+
 
 import './styles.css';
 
 function Coursos() {
     const [courses, setCourses] = useState([]);
+    const [images, setImages] = useState([]);
+
+    const wrapperRef = useRef(null);
+
+    useEffect(() => {
+
+        const currentImages = Array.from(wrapperRef.current.children);
+
+        setImages(currentImages);
+
+    }, []);
+
+    let time = 5000,
+        currentImageIndex = 0,
+        max = images.length;
 
     useEffect(() => {
         async function loadCourses() {
@@ -24,76 +41,45 @@ function Coursos() {
         }
 
         loadCourses();
+
     }, []);
+
+    function nextImage() {
+
+        // console.log(images[1])
+
+        // images[currentImageIndex].classList.remove("selected");
+
+        // currentImageIndex++
+
+        // if (currentImageIndex >= max)
+        //     currentImageIndex = 0
+
+        // images[currentImageIndex].classList.add("selected");
+    }
+
+    function start() {
+        setInterval(() => {
+            // troca de image
+            nextImage()
+        }, time)
+    }
+
+    window.addEventListener("load", start)
 
     return (
         <>
             <Header />
 
-            <div className="course-slider">
-                <ul className="slider">
-                    <a href="/courses">
-                        <li>
-                            <img src={arcade} />
-                        </li>
-                    </a>
-                </ul>
+            <div>
+                <div ref={wrapperRef} id="slider">
+                    <img className="selected" src={slide2} />
+                    <img src={slide2} />
+                </div>
             </div>
 
             <div className="course-title">
                 <h1>Bem-vindo(a) a GeekLoad</h1>
-            </div>
-
-            <h2 className="course-definition">
-                Cursos em destaque de <a href="">Desenvolvimento de Games</a>
-            </h2>
-            <div className="wrapper">
-                <section>
-                    <a href="#aventura-1587755079908.png">‹</a>
-                    {courses.map(course => (
-                        <Course key={course._id} course={course} />
-                    ))}
-                    <a href="#VINGADORES-ULTIMATO-2019-1587910404576.jpg">›</a>
-                </section>
-            </div>
-
-            <h2 className="course-definition">
-                Cursos em destaque de <a href="">Desenvolvimento de android</a>
-            </h2>
-            <div className="wrapper">
-                <section>
-                    <a href="#aventura-1587755079908.png">‹</a>
-                    {courses.map(course => (
-                        <Course key={course._id} course={course} />
-                    ))}
-                    <a href="#VINGADORES-ULTIMATO-2019-1587910404576.jpg">›</a>
-                </section>
-            </div>
-
-            <h2 className="course-definition">
-                Cursos em destaque de <a href="">Desenvolvimento de Games</a>
-            </h2>
-            <div className="wrapper">
-                <section>
-                    <a href="#aventura-1587755079908.png">‹</a>
-                    {courses.map(course => (
-                        <Course key={course._id} course={course} />
-                    ))}
-                    <a href="#VINGADORES-ULTIMATO-2019-1587910404576.jpg">›</a>
-                </section>
-            </div>
-
-            <h2 className="course-definition">
-                Cursos em destaque de <a href="">Desenvolvimento de Games</a>
-            </h2>
-            <div className="wrapper">
-                <section>
-                    <a href="#aventura-1587755079908.png">‹</a>
-                    {courses.map(course => (
-                        <Course key={course._id} course={course} />
-                    ))}
-                    <a href="#VINGADORES-ULTIMATO-2019-1587910404576.jpg">›</a>
-                </section>
             </div>
 
             <h2 className="course-definition">
