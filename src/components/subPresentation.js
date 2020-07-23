1d428501fad365c89af05924ed1f0d35f86f8d6e
f3l1p3v4 <fvaldez018@gmail.com>
2020-07-22 22:19:50 -0400

    update

--- src/components/subPresentation.js
+++ src/components/subPresentation.js
@@ -6,10 +6,10 @@ import credit from "../assets/credit.svg";
 class subPresentation extends Component {
     render() {
         return (
-            <div className="sub-presentation">
+            <div className="presentation">
                 <h1>Formas de pagamento</h1>
-                <section class="sub-presentation-section">
-                    <div className="sub-presentation-card">
+                <section class="presentation-section">
+                    <div className="presentation-card">
                         <div className="card-text">
                             <h1>Page com Boleto!</h1>
                             <h3>Seja avisado no seu e-mail quando um novo curso for lançado.</h3>
@@ -18,7 +18,7 @@ class subPresentation extends Component {
                             <img src={boleto} alt="boleto"/>
                         </div>  
                     </div>
-                    <div className="sub-presentation-card">
+                    <div className="presentation-card">
                         <div className="card-text">
                             <h1>Page com cartão!</h1>
                             <h3>Seja avisado no seu e-mail quando um novo curso for lançado.</h3>
@@ -27,7 +27,7 @@ class subPresentation extends Component {
                             <img src={credit} alt="credit"/>
                         </div> 
                     </div>
-                    <div className="sub-presentation-card">
+                    <div className="presentation-card">
                         <div className="card-text">
                             <h1>Page com cartão!</h1>
                             <h3>Seja avisado no seu e-mail quando um novo curso for lançado.</h3>
