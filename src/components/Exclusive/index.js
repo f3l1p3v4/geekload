dc66c13c471a151d0a799378c573f09a2c2245c3
Felipe Valdez <47610639+f3l1p3v4@users.noreply.github.com>
2021-04-09 08:48:27 -0400

    update

--- src/components/Exclusive/index.js
+++ src/components/Exclusive/index.js
@@ -1,6 +1,4 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import { FaArrowCircleRight } from "react-icons/fa";
 
 import notification from "../../assets/notificacao.png";
 
