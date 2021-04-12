2b5c2201c2dc057e2a2e7f6726cad60483c4f468
Felipe Valdez <47610639+f3l1p3v4@users.noreply.github.com>
2021-04-12 17:02:52 -0400

    update

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,24 +1,41 @@
 import React from "react";
+import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
+
+import logoTitle from "../../assets/icon-title.png";
 
 import "./styles.css";
 
 const Footer = () => (
   <footer>
+    <hr />
     <div className="social-container">
-      <a href="/about">
-        <p>Quem somos</p>
-      </a>
-      <a href="/">
-        <p>Termos</p>
-      </a>
-      <a href="/">
-        <p>Política de Privacidade</p>
-      </a>
+      <div className="footer-info">
+        <a href="/about">
+          <p>Quem somos</p>
+        </a>
+        <a href="/">
+          <p>Termos</p>
+        </a>
+        <a href="/">
+          <p>Política de Privacidade</p>
+        </a>
+      </div>
+      <div className="footer-social">
+        <a href="/about">
+          <FaFacebook />
+        </a>
+        <a href="/">
+          <FaInstagram />
+        </a>
+        <a href="/">
+          <FaTwitter />
+        </a>
+      </div>
     </div>
-    <p>
-      © 2020/{new Date().getFullYear()}, Copyright. Site desenvolvido por Felipe
-      Valdez
-    </p>
+    <div className="footer-img">
+      <img src={logoTitle} alt="Logo DevLoad" className="logo-title" />
+      <p>© 2020/{new Date().getFullYear()}, DevLoad.</p>
+    </div>
   </footer>
 );
 
