f64faffdefa38524e1dab16acedb495ad1b21690
Felipe Valdez <47610639+f3l1p3v4@users.noreply.github.com>
2021-08-12 10:32:13 -0400

    added efect

--- src/components/Presentation/index.js
+++ src/components/Presentation/index.js
@@ -8,16 +8,14 @@ class Presentation extends Component {
     return (
       <div className="presentation">
         <section className="presentation-container">
-          <div>
-            <h1>DevLoad</h1>
-            <h1>Faz você pensar fora da caixa.</h1>
-            <p>
-              Transforme a sua vida e a de milhares, milhões ou até bilhões de
-              pessoas com tecnologia.
-            </p>
-            <div>
-              <Link to="/course">Iniciar!</Link>
-            </div>
+          <h1 className="typing-animation">DevLoad</h1>
+          <h2>Faz você pensar fora da caixa.</h2>
+          <p>
+            Transforme a sua vida e a de milhares, milhões ou até bilhões de
+            pessoas com tecnologia.
+          </p>
+          <div className="presentation-button">
+            <Link to="/course">Iniciar!</Link>
           </div>
         </section>
       </div>
